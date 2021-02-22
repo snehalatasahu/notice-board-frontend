ddb5bac0210d12d017e4a125d946e06ec0fb4d2c
Snehalata Sahu <sneha16sahu@gmail.com>
2021-02-23 00:07:19 +0530

    Updates

---
+++ precache-manifest.9863445d7b8ef6c9e7f0fd9a08bcf671.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba174f35dc6dea24162a29691d4c9089",
+    "url": "/snehalatasahu/notice-board-frontend/index.html"
+  },
+  {
+    "revision": "4cf37f10d02365a6470e",
+    "url": "/snehalatasahu/notice-board-frontend/static/css/main.28d88270.chunk.css"
+  },
+  {
+    "revision": "38c5e4b3cc165baa203b",
+    "url": "/snehalatasahu/notice-board-frontend/static/js/2.411f45cc.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/snehalatasahu/notice-board-frontend/static/js/2.411f45cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cf37f10d02365a6470e",
+    "url": "/snehalatasahu/notice-board-frontend/static/js/main.2a57592f.chunk.js"
+  },
+  {
+    "revision": "dc7c91cda3a961150db1",
+    "url": "/snehalatasahu/notice-board-frontend/static/js/runtime-main.a35d9959.js"
+  },
+  {
+    "revision": "d44a30d645d6e8f821d70b0425349f5b",
+    "url": "/snehalatasahu/notice-board-frontend/static/media/Background.d44a30d6.jpg"
+  },
+  {
+    "revision": "5917c0d60cf03abd5813e673d385e611",
+    "url": "/snehalatasahu/notice-board-frontend/static/media/achievement.5917c0d6.jpg"
+  },
+  {
+    "revision": "3f9c600eb42a7726a4d85513e8bcbb3a",
+    "url": "/snehalatasahu/notice-board-frontend/static/media/activity.3f9c600e.jpg"
+  },
+  {
+    "revision": "3b943be949722b2d955b81212f4fe673",
+    "url": "/snehalatasahu/notice-board-frontend/static/media/exam.3b943be9.jpg"
+  },
+  {
+    "revision": "da4578b506c5b8928decc55d47707727",
+    "url": "/snehalatasahu/notice-board-frontend/static/media/image.da4578b5.jpg"
+  },
+  {
+    "revision": "e04d5a2828ed21680c9deada2b49156e",
+    "url": "/snehalatasahu/notice-board-frontend/static/media/logo.e04d5a28.png"
+  },
+  {
+    "revision": "95609bf0008ddbd6460211ca4323a834",
+    "url": "/snehalatasahu/notice-board-frontend/static/media/other.95609bf0.jpg"
+  },
+  {
+    "revision": "a3d7fda5a1a6884bc11083d0946ba286",
+    "url": "/snehalatasahu/notice-board-frontend/static/media/placement.a3d7fda5.jpg"
+  },
+  {
+    "revision": "dd32a56bf221781a9e65b2a7790e5440",
+    "url": "/snehalatasahu/notice-board-frontend/static/media/scholarship.dd32a56b.jpg"
+  },
+  {
+    "revision": "ca6e9b9b030076053bbc1aaf227ceec2",
+    "url": "/snehalatasahu/notice-board-frontend/static/media/whatsapp icon.ca6e9b9b.svg"
+  }
+]);
